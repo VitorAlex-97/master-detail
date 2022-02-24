e5b8a9552f75d17fddca4d67a4c308638abf7f23
VitorAlex-97 <vitualex97@gmail.com>
2022-02-24 10:04:29 -0300

    intercepting http requests with angular-in-memory-web-api

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -1,4 +1,3 @@
-import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, throwError } from 'rxjs';
