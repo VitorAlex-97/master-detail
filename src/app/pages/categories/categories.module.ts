f8b4ff5b0db704260a058d4406e3f6a60d4d1815
VitorAlex-97 <vitualex97@gmail.com>
2022-03-02 11:22:10 -0300

    adding BaseResourceModel

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 
