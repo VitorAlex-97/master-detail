605dcdf0465b39201414bb27049346899c3cd6da
VitorAlex-97 <vitualex97@gmail.com>
2022-03-03 12:06:49 -0300

    adding PageHeaderComponent to pages

---
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.css']
+})
+export class PageHeaderComponent implements OnInit {
+
+  @Input('page-tile') pageTitle: string;
+  @Input('button-class') buttonClass: string;
+  @Input('button-text') buttonTex: string;
+  @Input('button-link') buttonLink: string;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
