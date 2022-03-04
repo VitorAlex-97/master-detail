3508628fec0204fc01d00dd77448c41ee613fa63
VitorAlex-97 <vitualex97@gmail.com>
2022-03-04 09:36:46 -0300

    adding reports template

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
+  months = [
+    {month: 'janeiro', value: 1},
+    {month: 'fevereiro', value: 2},
+    {month: 'março', value: 3},
+    {month: 'abril', value: 4},
+    {month: 'maio', value: 5},
+    {month: 'junho', value: 6},
+    {month: 'julio', value: 7},
+    {month: 'agosto', value: 8},
+    {month: 'setembro', value: 9},
+    {month: 'outubro', value: 10},
+    {month: 'novembro', value: 11},
+    {month: 'dezembro', value: 12}
+  ];
+
+  years = [
+    {year: 2016},
+    {year: 2017},
+    {year: 2018},
+    {year: 2019},
+    {year: 2020}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  generateReports() {
+    
   }
 
 }
