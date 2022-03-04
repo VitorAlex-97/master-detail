a30742e37dcf1976a21ac61e1b1cbfb7126f1dd5
VitorAlex-97 <vitualex97@gmail.com>
2022-03-04 09:45:10 -0300

    improving PageHeaderComponent

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PageHeaderComponent implements OnInit {
 
   @Input('page-tile') pageTitle: string;
+  @Input('show-button') showButton: boolean = true;
   @Input('button-class') buttonClass: string;
   @Input('button-text') buttonTex: string;
   @Input('button-link') buttonLink: string;
